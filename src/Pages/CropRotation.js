c9a6dd11b82dbb8ef80aacb9c7690d40ea0f9361
Kaelin Shih <albertshih3@gmail.com>
2024-03-10 03:50:35 -0700

    brought back deleted croprotation

--- src/Pages/CropRotation.js
+++ src/Pages/CropRotation.js
@@ -1,8 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import axios from 'axios';
+import { useAuth0 } from "@auth0/auth0-react";
 // import './CropRotation.css';
 
+async function getUserFromJoinTable(userID) {
+  let data = JSON.stringify({
+    "dataSource": "Data0",
+    "database": "userData",
+    "collection": "join", // replace with your join table name
+    "filter": { "email": userID }
+  });
+
+  let config = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: 'http://localhost:3001/api/data',
+    headers: { 
+      'Content-Type': 'application/json', 
+      'Access-Control-Allow-Origin': '*', 
+      'api-key': 'jMoBVq9SZJleC59mT6ifNYa6cyHw3UHWXiLEWwRfizWVvaWbV7TTFwRUL6wqPOT3', 
+      'Accept': 'application/json'
+    },
+    data : data
+  };
+
+  try {
+    const response = await axios(config);
+    console.log(JSON.stringify(response.data));
+    return response.data.documents[0]; // Modify this line
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
 const CropRotation = () => {
+
+  // Get current user information
+  const { user, isAuthenticated, isLoading } = useAuth0();
+
   const [selectedCrop, setSelectedCrop] = useState('');
   const [rotationHistory, setRotationHistory] = useState([]);
 
@@ -16,6 +53,52 @@ const CropRotation = () => {
     setSelectedCrop('');
   };
 
+  // Define userId as a state
+const [userId, setUserId] = useState(null);
+
+// Fetch userId when component mounts
+useEffect(() => {
+  if (isAuthenticated) {
+    let data = JSON.stringify({
+      "dataSource": "Data0",
+      "database": "userData",
+      "collection": "users",
+      "filter": {"email" : user.email}
+    });
+
+    let config = {
+      method: 'post',
+      maxBodyLength: Infinity,
+      url: 'http://localhost:3001/api/data',
+      headers: { 
+        'Content-Type': 'application/json',
+        'api-key': 'jMoBVq9SZJleC59mT6ifNYa6cyHw3UHWXiLEWwRfizWVvaWbV7TTFwRUL6wqPOT3', 
+        'Accept': 'application/json'
+      },
+      data : data
+    };
+
+    axios.request(config)
+    .then((response) => {
+      console.log(JSON.stringify(response.data));
+      setUserId(response.data.documents[0].email); // Set userId here
+      console.log("userId: " + userId);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+}, [isAuthenticated, user.email]); // Depend on isAuthenticated and user.email
+
+// Fetch joinData when userId changes
+useEffect(() => {
+  if (userId) {
+    getUserFromJoinTable(userId);
+  }
+}, [userId]); // Depend on userId
+
+
+  
   return (
     <Container className="mt-4">
       <Row>
