ff1d8496deb6cff82e22e28755439ba6c138d304
Kaelin Shih <albertshih3@gmail.com>
2024-03-10 00:23:25 -0800

    WE CAN GET DATA NOW
    gets data from mongodb. We added a local server as a proxy to grab data using a post request.

--- src/Pages/CropRotation.js
+++ src/Pages/CropRotation.js
@@ -1,8 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import axios from 'axios';
 // import './CropRotation.css';
 
 const CropRotation = () => {
+
+  // Use API to get data from the join database
+  let data = JSON.stringify({
+    "dataSource": "Data0",
+    "database": "userData",
+    "collection": "join",
+    "filter": {}
+  });
+
+  let config = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: 'http://localhost:3001/api/data',
+    headers: { 
+      'Content-Type': 'application/json', 
+      'Access-Control-Allow-Origin': '*', 
+      'api-key': 'jMoBVq9SZJleC59mT6ifNYa6cyHw3UHWXiLEWwRfizWVvaWbV7TTFwRUL6wqPOT3', 
+      'Accept': 'application/json'
+    },
+    data : data
+  };
+
+  axios(config)
+  .then((response) => {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+  
+
   const [selectedCrop, setSelectedCrop] = useState('');
   const [rotationHistory, setRotationHistory] = useState([]);
 
