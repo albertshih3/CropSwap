1cc3f79d2e54e74963bdeafe5e2ab14d8b342b48
Kaelin Shih <albertshih3@gmail.com>
2024-03-09 18:06:19 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import Navbar from './Components/Navbar/Navbar';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './Pages/Home';
+import { createRoot } from 'react-dom/client';
 import { Auth0Provider } from '@auth0/auth0-react';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <Auth0Provider
       domain="dev-2qksybid6u0gy6vg.us.auth0.com"
       clientId="GnyjMW0GgzaM8akwiBtjHqYRp97n9MHa"
-      redirectUri={window.location.origin}
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
     >
+      <Navbar />
       <App />
     </Auth0Provider>,
-  document.getElementById('root')
-);
+  );
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
